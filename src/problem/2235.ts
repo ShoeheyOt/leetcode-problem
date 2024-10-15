7bd2c38c0bd75c75288c6c5f36e88fc3df90938c
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-10-15 11:09:23 -0700

    complete 2235

---
+++ src/problem/2235.ts
@@ -0,0 +1,13 @@
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+const num1 = 12;
+const num2 = 5;
+
+console.log(sum(num1, num2));
+
+const num3 = -10;
+const num4 = 4;
+
+console.log(sum(num3, num4));
